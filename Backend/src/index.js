965fb82f79ef4502d76d02180cd51fc2c60abc47
kishan dev sah <sonofsahuji@gmail.com>
2026-02-13 12:17:23 +0530

    cores updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -5,10 +5,10 @@ const cors = require('cors')
 const app = express();
 require('dotenv').config()
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }))
 
 app.use(express.json());
 
